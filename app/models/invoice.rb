680d16fb987b691338c1a60f153ab4367fe5d275
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-03 16:25:04 -0600

    Create test for invoice method and pass it

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,5 +4,13 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   has_many :transactions
-  
+
+
+
+  # In that section I see a list of the ids of all invoices
+  # That have items that have not yet been shipped
+
+  def self.unshipped_items
+    joins(:invoice_items).where('invoice_items.status != 2').select('invoices.*').group('invoices.id').distinct
+  end
 end
