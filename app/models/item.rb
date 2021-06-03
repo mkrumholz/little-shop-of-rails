85fd3141bb354507796837068fa2d5f3188493a0
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-02 19:50:48 -0600

    Create #display_price method for Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,11 @@
-class Item < ApplicationRecord
 
+class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
 
+  def display_price
+    price = (BigDecimal(unit_price)/100).to_f
+    sprintf("$%#.2f", price)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe Item do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should belong_to(:merchant) }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @merchant = Merchant.create!(name: "Little Shop of Horrors")
+      @item_1 = @merchant.items.create!(name: 'Audrey II', description: 'Large, man-eating plant', unit_price: '100000000')
+    end
+
+    describe '#display_price' do
+      it 'displays the price of the item in dollars' do
+        expect(@item_1.display_price).to eq '$1000000.00'
+      end
+    end
+  end
 end
