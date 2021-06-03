7d86bc865fe5cd993591cdc3c6304d8e689b27d6
Aliya Merali <merali4@gmail.com>
2021-06-03 17:08:12 -0600

    Add merchant instance method to render status based on boolean field

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
 
   has_many :items, dependent: :destroy
 
+  def render_status
+    if self.status
+      "Enabled"
+    else
+      "Disabled"
+    end
+  end
+
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Merchant do
     it { should have_many(:items).dependent(:destroy) }
 
   end
+
+  describe 'instance methods' do
+    it '#render_status returns Enabled or Disabled based on boolean status' do
+      tees = Merchant.create!(name: "T-shirts by Terry", status: true)
+      amphs = Merchant.create!(name: "All About Amphibians", status: false)
+
+      expect(tees.render_status).to eq("Enabled")
+      expect(amphs.render_status).to eq("Disabled")
+    end
+  end
 end
