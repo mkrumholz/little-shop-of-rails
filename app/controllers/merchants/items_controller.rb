422da3779d87b87dbd226976af05534f54aa8770
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-02 19:51:42 -0600

    Create basic show page with item details

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,6 +1,11 @@
 class Merchants::ItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    @items = Item.where(merchant_id: merchant.id)
+    @merchant = Merchant.find(params[:merchant_id])
+    @items = Item.where(merchant_id: @merchant.id)
+  end
+
+  def show
+    @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
   end
 end
