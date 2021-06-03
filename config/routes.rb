04dff4f1ac30101430b924dad77af71a584cf4d5
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-02 19:12:57 -0600

    Create basic merchant items index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchants, only: [:show] do
-    resources :items, only: [:index, :show]
+  resources :merchants, only: :show do
+    scope module: :merchants do
+      resources :items, only: [:index, :show]
+    end
   end
 end
--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'The merchant items index' do
   it 'lists all of the items' do
-    merchant = Merchant.create!(name: "Susan")
-    item = merchant.items.create!(name: 'glass', description: 'clear', unit_price: '1400')
+    merchant = Merchant.create!(name: "Little Shop of Horrors")
+    item_1 = merchant.items.create!(name: 'Audrey II', description: 'Large, man-eating plant', unit_price: '100000000')
+    item_2 = merchant.items.create!(name: 'Bouquet of roses', description: '12 red roses', unit_price: '1900')
+    item_3 = merchant.items.create!(name: 'Orchid', description: 'Purple, 3 inches', unit_price: '2700')
+    item_4 = merchant.items.create!(name: 'Echivaria', description: 'Peacock varietal', unit_price: '3100')
+
     visit "/merchants/#{merchant.id}/items"
 
-    expect(page).to have_content item.name
+    expect(page).to have_content 'Audrey II'
+    expect(page).to have_content 'Bouquet of roses'
+    expect(page).to have_content 'Orchid'
+    expect(page).to have_content 'Echivaria'
   end
 end
