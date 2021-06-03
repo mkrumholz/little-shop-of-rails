8e3be1a66c44586b5ea5ca4b52b030b062a4526e
Aliya Merali <merali4@gmail.com>
2021-06-02 18:49:01 -0600

    Add css id and within blocks for testing

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -10,15 +10,19 @@ RSpec.describe 'Admin Merchants Index' do
   end
 
   it 'shows the names of each merchant in the system' do
-    expect(page).to have_content(@signs.name)
-    expect(page).to have_content(@tees.name)
-    expect(page).to have_content(@amphs.name)
+    within("#merchant-list") do
+      expect(page).to have_content(@signs.name)
+      expect(page).to have_content(@tees.name)
+      expect(page).to have_content(@amphs.name)
+    end
   end
 
   it 'has a link to the admin merchant show page in each merchant name' do
-    expect(page).to have_link("#{@signs.name}", :href=>"/admin/merchants/#{@signs.id}")
-    expect(page).to have_link("#{@tees.name}", :href=>"/admin/merchants/#{@tees.id}")
-    expect(page).to have_link("#{@amphs.name}", :href=>"/admin/merchants/#{@amphs.id}")
+    within("#merchant-list") do
+      expect(page).to have_link("#{@signs.name}", :href=>"/admin/merchants/#{@signs.id}")
+      expect(page).to have_link("#{@tees.name}", :href=>"/admin/merchants/#{@tees.id}")
+      expect(page).to have_link("#{@amphs.name}", :href=>"/admin/merchants/#{@amphs.id}")
+    end
   end
 
   it 'link directs to show page' do
