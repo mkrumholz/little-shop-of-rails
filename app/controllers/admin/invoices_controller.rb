25b65425534ea55d327a6b6d086817151d98c6ab
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-03 23:42:24 -0600

    Revert "Add links and ids to admin invoice index"
    This reverts commit 12a64af82ad4054da1a40ead036cf6e8a7bbad29.

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -1,11 +1,9 @@
 class Admin::InvoicesController < ApplicationController
 
   def index
-    @invoices = Invoice.all.order(:id)
   end
 
   def show
-    @invoice = Invoice.find(params[:id])
   end
 
 end
