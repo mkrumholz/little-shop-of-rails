0859569e1789ea276539f1c3226752a136d6ea0c
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-03 20:00:58 -0600

    Add validations to Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   belongs_to :merchant
+  validates_presence_of :name, :description, :unit_price
+  validates_inclusion_of :enabled, in: [true, false]
 
   def display_price
     price = (BigDecimal(unit_price)/100).to_f
