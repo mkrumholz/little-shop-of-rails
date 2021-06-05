2b109d8d7866824fafe6be4511c7828e44d11037
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-05 13:46:23 -0600

    Build method for finding top 5 items by revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,14 @@ class Item < ApplicationRecord
   def self.disabled_only
     where(enabled: false)
   end
+
+  def self.top_5_by_revenue 
+    joins(:invoice_items, :invoices)
+    .select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+    .joins('inner join transactions on transactions.invoice_id=invoices.id')
+    .where(transactions: {result: 1})
+    .group(:id)
+    .order('revenue desc')
+    .limit(5)
+  end
 end
