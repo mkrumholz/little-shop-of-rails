5aaf19ffbd22e14cd057d74aa795f6517ea7be93
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-06-06 17:20:25 -0600

    Invoice Item Info feature test complete

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -34,12 +34,58 @@ RSpec.describe 'Merchant Invoices Show Page' do
       invoice_item_4 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_4.id, status: 1)
 
       visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-      
+
       expect(page).to have_content(invoice_1.id)
       expect(page).to have_content(invoice_1.status)
       expect(page).to have_content(invoice_1.created_at.strftime('%A, %B %d, %Y'))
       expect(page).to have_content(customer_1.first_name)
       expect(page).to have_content(customer_1.last_name)
+    end
+
+    it 'can see all of that merchants invoice item info' do
+      # Merchant Invoice Show Page: Invoice Item Information
+      # As a merchant
+      # When I visit my merchant invoice show page
+      # Then I see all of my items on the invoice including:
+      # - Item name
+      # - The quantity of the item ordered
+      # - The price the Item sold for
+      # - The Invoice Item status
+      # And I do not see any information related to Items for other merchants
+      merchant = Merchant.create!(name: 'Schroeder-Jerde')
+      merchant_2 = Merchant.create!(name: 'James Bond')
+      customer_2 = Customer.create!(first_name: 'Evan', last_name: 'East')
+      customer_3 = Customer.create!(first_name: 'Yasha', last_name: 'West')
+      customer_1 = Customer.create!(first_name: 'Sally', last_name: 'Shopper')
+      customer_4 = Customer.create!(first_name: 'Du', last_name: 'North')
+      customer_5 = Customer.create!(first_name: 'Jackie', last_name: 'Chan')
+      item_1 = merchant.items.create!(name: 'Gold Ring', description: 'Jewelery', unit_price: 10000)
+      item_4 = merchant.items.create!(name: 'Hair Clip', description: 'Accessories', unit_price: 200)
+      item_2 = merchant.items.create!(name: 'Silver Ring', description: 'Jewelery', unit_price: 5000)
+      item_3 = merchant.items.create!(name: 'Hoop Earrings', description: 'Jewelery', unit_price: 1000)
+      item_5 = merchant_2.items.create!(name: 'Silver Bracelet', description: 'Accessories', unit_price: 3000)
+      item_6 = merchant_2.items.create!(name: 'Bronze Ring', description: 'Jewelery', unit_price: 2000)
+      invoice_1 = customer_1.invoices.create!(status: 1, created_at: "2012-03-06 14:54:15 UTC")
+      invoice_4 = customer_4.invoices.create!(status: 1, created_at: "2012-03-09 14:54:15 UTC")
+      invoice_2 = customer_2.invoices.create!(status: 1, created_at: "2012-03-07 00:54:24 UTC")
+      invoice_3 = customer_3.invoices.create!(status: 1, created_at: "2012-03-08 14:54:15 UTC")
+      invoice_5 = customer_5.invoices.create!(status: 1, created_at: "2012-03-10 14:54:15 UTC")
+      invoice_6 = customer_5.invoices.create!(status: 1, created_at: "2012-03-11 14:54:15 UTC")
+      invoice_item_1 = InvoiceItem.create!(quantity: 2, unit_price: 10000, item_id: item_1.id, invoice_id: invoice_1.id, status: 1)
+      invoice_item_2 = InvoiceItem.create!(quantity: 2, unit_price: 5000, item_id: item_2.id, invoice_id: invoice_1.id, status: 1)
+      invoice_item_3 = InvoiceItem.create!(quantity: 2, unit_price: 1000,item_id: item_3.id, invoice_id: invoice_1.id, status: 1)
+      invoice_item_4 = InvoiceItem.create!(quantity: 2, unit_price: 200,item_id: item_4.id, invoice_id: invoice_4.id, status: 1)
+      invoice_item_5 = InvoiceItem.create!(quantity: 2, unit_price: 3000,item_id: item_5.id, invoice_id: invoice_5.id, status: 1)
+      invoice_item_6 = InvoiceItem.create!(quantity: 2, unit_price: 2000,item_id: item_6.id, invoice_id: invoice_6.id, status: 2)
+
+
+      visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
+
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_content(invoice_item_1.quantity)
+      expect(page).to have_content(invoice_item_1.unit_price)
+      expect(page).to have_content(invoice_item_1.status)
+      expect(page).to_not have_content(item_5.name)
     end
   end
 end
