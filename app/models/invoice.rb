bf313a96581537bfd0cfec3d0c263447f71bb3fd
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-06 16:05:03 -0600

    Update Item method test setup locations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -16,11 +16,11 @@ class Invoice < ApplicationRecord
   def self.highest_revenue_date(item_id)
     joins(:invoice_items)
     .joins('right join transactions on transactions.invoice_id=invoices.id')
-    .select('invoices.updated_at as date')
+    .select(invoices: :updated_at)
     .where(invoice_items: {item_id: item_id}, transactions: {result: 1}, invoices: {status: 1})
     .order(Arel.sql('sum(invoice_items.quantity * invoice_items.unit_price) desc, invoices.updated_at desc'))
-    .group('invoices.updated_at')
+    .group(:updated_at)
     .limit(1)
-    .pluck('invoices.updated_at')
+    .pluck(:updated_at)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,39 +13,40 @@ RSpec.describe Item do
     it { should validate_presence_of :unit_price }
   end
   
+  before :each do
+    @merchant = Merchant.create!(name: "Little Shop of Horrors")
+    @customer = Customer.create!(first_name: 'Audrey', last_name: 'I')
+
+    @item_1 = @merchant.items.create!(name: 'Audrey II', description: 'Large, man-eating plant', unit_price: '100000000', enabled: true)
+    @item_2 = @merchant.items.create!(name: 'Bouquet of roses', description: '12 red roses', unit_price: '1900', enabled: true)
+    @item_3 = @merchant.items.create!(name: 'Orchid', description: 'Purple, 3 inches', unit_price: '2700', enabled: false)
+    @item_4 = @merchant.items.create!(name: 'Echevaria', description: 'Peacock varietal', unit_price: '3100', enabled: true)
+    @item_5 = @merchant.items.create!(name: 'Fourth item', description: '4th best', unit_price: '26400', enabled: true)
+    @item_6 = @merchant.items.create!(name: 'Fifth item', description: '5th best', unit_price: '2400', enabled: true)
+    @item_7 = @merchant.items.create!(name: 'Sixth item', description: '6th best', unit_price: '50', enabled: true)
+
+    @invoice_1 = @customer.invoices.create!(status: 1, updated_at: Date.parse("2021-03-01")) # is successful and paid
+    @invoice_2 = @customer.invoices.create!(status: 0, updated_at: Date.parse("2021-03-01")) # is cancelled
+    @invoice_3 = @customer.invoices.create!(status: 2, updated_at: Date.parse("2021-03-01")) # is still in progress, no good transactions
+    @invoice_4 = @customer.invoices.create!(status: 1, updated_at: Date.parse("2021-02-08")) # is successful and paid
+    @invoice_5 = @customer.invoices.create!(status: 1, updated_at: Date.parse("2021-02-01")) # has no successful transaction
+
+    @invoice_item_1 = @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 2, unit_price: 5000, status: 0)
+    @invoice_item_2 = @item_2.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 2, unit_price: 2500, status: 0)
+    @invoice_item_3 = @item_4.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 2, unit_price: 1000, status: 0)
+    @invoice_item_4 = @item_1.invoice_items.create!(invoice_id: @invoice_2.id, quantity: 2, unit_price: 5000, status: 0)
+    @invoice_item_5 = @item_1.invoice_items.create!(invoice_id: @invoice_3.id, quantity: 2, unit_price: 5000, status: 0)
+    @invoice_item_6 = @item_5.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 2, unit_price: 500, status: 0)
+    @invoice_item_7 = @item_6.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 2, unit_price: 200, status: 0)
+    @invoice_item_8 = @item_7.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 2, unit_price: 50, status: 0)
+
+    @invoice_1.transactions.create!(result: 1, credit_card_number: '534', credit_card_expiration_date: 'null')
+    @invoice_2.transactions.create!(result: 1, credit_card_number: '534', credit_card_expiration_date: 'null')
+    @invoice_4.transactions.create!(result: 1, credit_card_number: '534', credit_card_expiration_date: 'null')
+    @invoice_5.transactions.create!(result: 0, credit_card_number: '534', credit_card_expiration_date: 'null')
+  end
+
   describe 'class methods' do
-    before :each do
-      @merchant = Merchant.create!(name: "Little Shop of Horrors")
-      @customer = Customer.create!(first_name: 'Audrey', last_name: 'I')
-
-      @item_1 = @merchant.items.create!(name: 'Audrey II', description: 'Large, man-eating plant', unit_price: '100000000', enabled: true)
-      @item_2 = @merchant.items.create!(name: 'Bouquet of roses', description: '12 red roses', unit_price: '1900', enabled: true)
-      @item_3 = @merchant.items.create!(name: 'Orchid', description: 'Purple, 3 inches', unit_price: '2700', enabled: false)
-      @item_4 = @merchant.items.create!(name: 'Echevaria', description: 'Peacock varietal', unit_price: '3100', enabled: true)
-      @item_5 = @merchant.items.create!(name: 'Fourth item', description: '4th best', unit_price: '26400', enabled: true)
-      @item_6 = @merchant.items.create!(name: 'Fifth item', description: '5th best', unit_price: '2400', enabled: true)
-      @item_7 = @merchant.items.create!(name: 'Sixth item', description: '6th best', unit_price: '50', enabled: true)
-
-      @invoice_1 = @customer.invoices.create!(status: 1, updated_at: Date.parse("2021-03-01")) # is successful and paid
-      @invoice_2 = @customer.invoices.create!(status: 0, updated_at: Date.parse("2021-03-01")) # is cancelled
-      @invoice_3 = @customer.invoices.create!(status: 2, updated_at: Date.parse("2021-03-01")) # is still in progress, no good transactions
-      @invoice_4 = @customer.invoices.create!(status: 1, updated_at: Date.parse("2021-02-08")) # is successful and paid
-      @invoice_5 = @customer.invoices.create!(status: 1, updated_at: Date.parse("2021-02-01")) # has no successful transaction
-
-      @invoice_item_1 = @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 2, unit_price: 5000, status: 0)
-      @invoice_item_2 = @item_2.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 2, unit_price: 2500, status: 0)
-      @invoice_item_3 = @item_4.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 2, unit_price: 1000, status: 0)
-      @invoice_item_4 = @item_1.invoice_items.create!(invoice_id: @invoice_2.id, quantity: 2, unit_price: 5000, status: 0)
-      @invoice_item_5 = @item_1.invoice_items.create!(invoice_id: @invoice_3.id, quantity: 2, unit_price: 5000, status: 0)
-      @invoice_item_6 = @item_5.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 2, unit_price: 500, status: 0)
-      @invoice_item_7 = @item_6.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 2, unit_price: 200, status: 0)
-      @invoice_item_8 = @item_7.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 2, unit_price: 50, status: 0)
-
-      @invoice_1.transactions.create!(result: 1, credit_card_number: '534', credit_card_expiration_date: 'null')
-      @invoice_4.transactions.create!(result: 1, credit_card_number: '534', credit_card_expiration_date: 'null')
-      @invoice_5.transactions.create!(result: 0, credit_card_number: '534', credit_card_expiration_date: 'null')
-    end
-
     describe '.enabled_only' do
       it 'returns only the items where enabled = true' do
         expect(Item.enabled_only.length).to eq 6
@@ -72,25 +73,19 @@ RSpec.describe Item do
         expect(Item.top_5_by_revenue).to_not include @item_3
         expect(Item.top_5_by_revenue).to_not include @item_7
       end
-    end
+    end 
   end
 
   describe 'instance methods' do
-    before :each do
-      @merchant = Merchant.create!(name: "Little Shop of Horrors")
-      @item_1 = @merchant.items.create!(name: 'Audrey II', description: 'Large, man-eating plant', unit_price: '100000000')
-    end
-
     describe '#price_to_dollars' do
       it 'displays the price of the item in dollars' do
         expect(@item_1.price_to_dollars).to eq 1000000.0
       end
     end
-
-    # describe '#best_revenue_date' do
-    #   it 'returns the date on which the most revenue was earned for the item' do
-    #     expect(@item_1.best_revenue_date).to eq Date.parse('2021-03-01')
-    #   end
-    # end
+    describe '#best_revenue_date' do
+      it 'returns the date on which the most revenue was earned for the item' do
+        expect(@item_1.best_revenue_date[0]).to eq Date.parse('2021-03-01')
+      end
+    end
   end
 end
