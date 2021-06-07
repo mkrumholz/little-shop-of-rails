1db126c9f6bcb950e8c9f59049b5322fdc879671
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-07 17:33:04 -0600

    Refactor rspec tests for invoice validation

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -11,18 +11,19 @@ RSpec.describe 'Merchant Invoices Index' do
       # And each id links to the merchant invoice show page
       merchant = Merchant.create!(name: 'Schroeder-Jerde')
       merchant_2 = Merchant.create!(name: 'James Bond')
+      customer_2 = Customer.create!(first_name: 'Evan', last_name: 'East')
       item_1 = merchant.items.create!(name: 'Gold Ring', description: 'Jewelery', unit_price: 10000)
       item_4 = merchant.items.create!(name: 'Hair Clip', description: 'Accessories', unit_price: 200)
       item_2 = merchant.items.create!(name: 'Silver Ring', description: 'Jewelery', unit_price: 5000)
       item_3 = merchant.items.create!(name: 'Hoop Earrings', description: 'Jewelery', unit_price: 1000)
       item_5 = merchant_2.items.create!(name: 'Silver Bracelet', description: 'Accessories', unit_price: 3000)
       item_6 = merchant_2.items.create!(name: 'Bronze Ring', description: 'Jewelery', unit_price: 2000)
-      invoice_1 = item_1.invoices.create!(status: 1, created_at: "2012-03-06 14:54:15 UTC")
-      invoice_4 = item_4.invoices.create!(status: 1, created_at: "2012-03-09 14:54:15 UTC")
-      invoice_2 = item_2.invoices.create!(status: 1, created_at: "2012-03-07 00:54:24 UTC")
-      invoice_3 = item_3.invoices.create!(status: 1, created_at: "2012-03-08 14:54:15 UTC")
-      invoice_5 = item_5.invoices.create!(status: 1, created_at: "2012-03-10 14:54:15 UTC")
-      invoice_6 = item_6.invoices.create!(status: 1, created_at: "2012-03-11 14:54:15 UTC")
+      invoice_1 = customer_2.invoices.create!(status: 1, created_at: "2012-03-06 14:54:15 UTC")
+      invoice_4 = customer_2.invoices.create!(status: 1, created_at: "2012-03-09 14:54:15 UTC")
+      invoice_2 = customer_2.invoices.create!(status: 1, created_at: "2012-03-07 00:54:24 UTC")
+      invoice_3 = customer_2.invoices.create!(status: 1, created_at: "2012-03-08 14:54:15 UTC")
+      invoice_5 = customer_2.invoices.create!(status: 1, created_at: "2012-03-10 14:54:15 UTC")
+      invoice_6 = customer_2.invoices.create!(status: 1, created_at: "2012-03-11 14:54:15 UTC")
       invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, status: 1)
       invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, status: 1)
       invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_3.id, status: 1)
@@ -31,7 +32,7 @@ RSpec.describe 'Merchant Invoices Index' do
       invoice_item_6 = InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_6.id, status: 2)
 
       visit "/merchants/#{merchant.id}/invoices"
-      
+
       expect(page).to have_content(invoice_1.id)
       expect(page).to have_link("Invoice ##{invoice_1.id}")
       expect(page).to have_content(invoice_2.id)
