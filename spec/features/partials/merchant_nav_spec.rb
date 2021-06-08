4d19b758bce2c5f616e0121c2c19c37219093e7f
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-07 22:22:06 -0600

    Complete testing for nav partials

---
+++ spec/features/partials/merchant_nav_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'admin header nav' do
+  before :each do
+    @merchant = Merchant.create!(name: 'Schroeder-Jerde')
+  end
+
+  it 'links to the admin dashboard' do
+    visit "/merchants/#{@merchant.id}/dashboard"
+
+    expect(page).to have_link('Dashboard')
+    click_link('Dashboard')
+
+    expect(page).to have_current_path("/merchants/#{@merchant.id}/dashboard")
+  end
+
+  it 'links to the admin merchants index' do
+  visit "/merchants/#{@merchant.id}/dashboard"
+
+    expect(page).to have_link('My Items')
+    click_link('My Items')
+
+    expect(page).to have_current_path("/merchants/#{@merchant.id}/items")
+  end
+  
+  it 'links to the admin invoices index' do
+  visit "/merchants/#{@merchant.id}/dashboard"
+
+    expect(page).to have_link('My Invoices')
+    click_link('My Invoices')
+
+    expect(page).to have_current_path("/merchants/#{@merchant.id}/invoices")
+  end
+end
