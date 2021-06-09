f2bc6e6b6c9ba31e1c1f56781992c610686aa9a3
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-09 11:29:05 -0600

    Fix attribution of keys in github contributors

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,7 +4,7 @@ class GithubService
     response = Faraday.get "https://api.github.com/repos/LawrenceWhalen/little-esty-shop"
 
     body = response.body
-    JSON.parse(body, symbolize_names: true)
+    test = JSON.parse(body, symbolize_names: true)
   end
 
   def self.contributors_info
