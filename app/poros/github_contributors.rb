9e3ad05f62d3f2ef2356d8a5730c838287e5cbf8
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-09 16:31:55 -0600

    Fix test case sensetive

--- app/poros/github_contributors.rb
+++ app/poros/github_contributors.rb
@@ -2,19 +2,10 @@ class GithubContributors
 
   def self.contributors_info
     github_contributor_data = GithubService.contributors_info
-<<<<<<< Updated upstream
     github_contributor_data.each_with_object({}) do |contributor, hash|
       if [26797256, 78388882, 5446926, 78574189].include?(contributor[:id])
         hash[contributor[:id]] = {name: contributor[:login], contributions: contributor[:contributions]}
       end
     end
-
-=======
-      github_contributor_data.each_with_object({}) do |contributor, hash|
-        if [26797256, 78388882, 5446926, 78574189].include?(contributor[:id])
-          hash[contributor[:id]] = {name: contributor[:login], contributions: contributor[:contributions]}
-        end
-      end
->>>>>>> Stashed changes
   end
 end
