0557a3ae0ca98e475a7965c3eaa017c3f5a63552
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-09 16:47:35 -0600

    Welcome page testing

--- app/poros/github_contributors.rb
+++ app/poros/github_contributors.rb
@@ -2,15 +2,14 @@ class GithubContributors
 
   def self.contributors_info
     github_contributor_data = GithubService.contributors_info
-    require "pry"; binding.pry
-    if github_contributor_data[:message] != nil
+    if !github_contributor_data[0].keys.include?('message')
       github_contributor_data.each_with_object({}) do |contributor, hash|
         if [26797256, 78388882, 5446926, 78574189].include?(contributor[:id])
           hash[contributor[:id]] = {name: contributor[:login], contributions: contributor[:contributions]}
         end
       end
     else
-      print "API rate limit exceeded for 173.232.242.12. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)"
+      print "API rate limit exceeded."
     end
   end
 end
--- spec/features/admin/welcome_spec.rb
+++ spec/features/admin/welcome_spec.rb
@@ -1,13 +1,54 @@
 require 'rails_helper'
 
-Rspec.describe 'Welcome page' do
+RSpec.describe 'Welcome page' do
   before :each do
+
+    allow(GithubService).to receive(:contributors_info).and_return([
+      {id: 26797256, name: 'Molly', contributions: 7},
+      {id: 78388882, name: 'Sa', contributions: 80}
+    ])
+    allow(GithubService).to receive(:closed_pulls).and_return([
+      {id: 0101010011, name: 'Molly', merged_at: 7},
+      {id: 01011230011, name: 'Sa',merged_at: 80},
+      {id: 01011230011, name: 'Sa', merged_at: nil}
+    ])
+    allow(GithubService).to receive(:repo_info).and_return({
+        name: 'little-esty-shop'
+    })
+
     @merchant_1 = Merchant.create!(name: 'Tims my time', status: false)
     @merchant_2 = Merchant.create!(name: 'Future Fun', status: false)
     @merchant_3 = Merchant.create!(name: 'Dozen a Dime', status: false)
-    @merchant_4 = Merchant.create!(name: 'Fools Errant', status: false)
+
+    visit '/'
   end
   describe 'visit' do
-     
+    it 'has a button to the admin index' do
+      expect(page).to have_link('Admin Index')
+
+      click_link('Admin Index')
+
+      expect(page).to have_current_path('/admin')
+    end
+
+    it 'displays a selection of merchants' do
+      expect(page).to have_content("Merchant ##{@merchant_1.id}")
+      expect(page).to have_content("Merchant ##{@merchant_2.id}")
+      expect(page).to have_content("Merchant ##{@merchant_3.id}")
+
+      expect(page).to have_content("#{@merchant_1.name}")
+      expect(page).to have_content("#{@merchant_2.name}")
+      expect(page).to have_content("#{@merchant_3.name}")
+    end
+
+    it 'provides links to merchant dashboards' do
+      expect(page).to have_link("Merchant ##{@merchant_1.id}")
+      expect(page).to have_link("Merchant ##{@merchant_2.id}")
+      expect(page).to have_link("Merchant ##{@merchant_3.id}")
+
+      click_link("Merchant ##{@merchant_1.id}")
+
+      expect(page).to have_current_path("/merchants/#{@merchant_1.id}/dashboard")
+    end
   end
 end
