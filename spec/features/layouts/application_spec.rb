cfc90cc4f2ff31feea85357cf7fa1cc6c0f1b7a6
Lawrence Whalen <ldwhalen@gmail.com>
2021-06-09 11:58:41 -0600

    Fix errors in footer tests

--- spec/features/layouts/application_spec.rb
+++ spec/features/layouts/application_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'footer' do
   before :each do
     allow(GithubService).to receive(:contributors_info).and_return([
-      {id: 26797256, name: 'Molly', contributions: 7},
-      {id: 78388882, name: 'Sa', contributions: 80}
+      {id: 26797256, login: 'Molly', contributions: 7},
+      {id: 78388882, login: 'Sa', contributions: 80}
     ])
     allow(GithubService).to receive(:closed_pulls).and_return([
-      {id: 0101010011, name: 'Molly', merged_at: 7},
-      {id: 01011230011, name: 'Sa',merged_at: 80},
-      {id: 01011230011, name: 'Sa', merged_at: nil}
+      {id: '0101010011', name: 'Molly', merged_at: 7},
+      {id: '01011230011', name: 'Sa',merged_at: 80},
+      {id: '01011230011', name: 'Sa', merged_at: nil}
     ])
     allow(GithubService).to receive(:repo_info).and_return({
         name: 'little-esty-shop'
--- spec/features/partials/admin_nav_spec.rb
+++ spec/features/partials/admin_nav_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'admin header nav' do
-  it 'links to the admin dashboard' do
+  before :each do
+    allow(GithubService).to receive(:contributors_info).and_return([
+      {id: 26797256, name: 'Molly', contributions: 7},
+      {id: 78388882, name: 'Sa', contributions: 80}
+    ])
+    allow(GithubService).to receive(:closed_pulls).and_return([
+      {id: 0101010011, name: 'Molly', merged_at: 7},
+      {id: 01011230011, name: 'Sa',merged_at: 80},
+      {id: 01011230011, name: 'Sa', merged_at: nil}
+    ])
+    allow(GithubService).to receive(:repo_info).and_return({
+        name: 'little-esty-shop'
+    })
+
     visit '/admin'
+  end
+  it 'links to the admin dashboard' do
 
     expect(page).to have_link('Dashboard')
     click_link('Dashboard')
@@ -11,16 +26,14 @@ RSpec.describe 'admin header nav' do
   end
 
   it 'links to the admin merchants index' do
-    visit '/admin'
 
     expect(page).to have_link('Merchants')
     click_link('Merchants')
 
     expect(page).to have_current_path('/admin/merchants')
   end
-  
+
   it 'links to the admin invoices index' do
-    visit '/admin'
 
     expect(page).to have_link('Invoices')
     click_link('Invoices')
