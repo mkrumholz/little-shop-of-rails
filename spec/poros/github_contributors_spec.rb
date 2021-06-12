1aae65853fdb350f37c779d8e73f58755c7c5315
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-12 12:35:20 -0600

    Run rubocop on POROs

--- spec/poros/github_contributors_spec.rb
+++ spec/poros/github_contributors_spec.rb
@@ -5,31 +5,32 @@ RSpec.describe GithubContributors do
     describe '#contributor_info' do
       it 'returns a hash with key of id and value of contributor info' do
         allow(GithubService).to receive(:contributors_info).and_return([
-          {
-           id: 26797256,
-           name: 'Molly',
-           contributions: 7
-           },
-          {
-           id: 5446926,
-           name: 'Sa,',
-           contributions: 80
-          }
-        ])
+                                                                         {
+                                                                           id: 26_797_256,
+                                                                           name: 'Molly',
+                                                                           contributions: 7
+                                                                         },
+                                                                         {
+                                                                           id: 5_446_926,
+                                                                           name: 'Sa,',
+                                                                           contributions: 80
+                                                                         }
+                                                                       ])
 
-        expect(GithubContributors.contributors_info.keys.first).to eq(26797256)
+        expect(GithubContributors.contributors_info.keys.first).to eq(26_797_256)
         expect(GithubContributors.contributors_info.values.first.class).to eq(Hash)
       end
 
       it 'returns an error hash if api limit exceeded' do
         allow(GithubService).to receive(:contributors_info).and_return(
           {
-          :message=>"API rate limit exceeded for 98.38.149.214. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
-          :documentation_url=>"https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"}
+            :message => "API rate limit exceeded for 98.38.149.214. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
+            :documentation_url => 'https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting'
+          }
         )
 
-        expect(GithubContributors.contributors_info[0][1][:name]).to eq("Unavailable: API rate limit exceeded.")
-        expect(GithubContributors.contributors_info[0][1][:contributions]).to eq("Unavailable: API rate limit exceeded.")
+        expect(GithubContributors.contributors_info[0][1][:name]).to eq('Unavailable: API rate limit exceeded.')
+        expect(GithubContributors.contributors_info[0][1][:contributions]).to eq('Unavailable: API rate limit exceeded.')
       end
     end
   end
--- spec/poros/github_repo_spec.rb
+++ spec/poros/github_repo_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe GithubRepo do
   describe '.name' do
     it 'returns the name of the repo' do
       allow(GithubService).to receive(:repo_info).and_return({
-          name: 'little-esty-shop'
-      })
+                                                               name: 'little-esty-shop'
+                                                             })
 
       expect(GithubRepo.name_info).to eq('little-esty-shop')
     end
     it 'returns an error if the api limit is exceeded' do
       allow(GithubService).to receive(:repo_info).and_return({
-          message: 'error',
-          name: 'little-esty-shop'
-      })
+                                                               message: 'error',
+                                                               name: 'little-esty-shop'
+                                                             })
 
-      expect(GithubRepo.name_info).to eq("Unavailable: API rate limit exceeded.")
+      expect(GithubRepo.name_info).to eq('Unavailable: API rate limit exceeded.')
     end
   end
 end
