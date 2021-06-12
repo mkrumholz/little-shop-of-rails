25d72549a831d1a8c74a5e423ef5911e3b0fb00b
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-12 14:57:15 -0600

    Write test for merchant discount show page

---
+++ spec/features/merchants/discounts/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'discount show page' do
+  it 'displays the name and details for the discount' do
+    merchant_1 = FactoryBot.create(:merchant)
+    discount_1 = merchant_1.discounts.create!(name: '4 or More', percentage: 0.1000, quantity_threshold: 4)
+    discount_2 = merchant_1.discounts.create!(name: '20% for 10', percentage: 0.2000, quantity_threshold: 10)
+
+    visit "/merchants/#{merchant_1.id}/discounts/#{discount_1.id}"
+
+    expect(page).to have_content discount_1.name
+    expect(page).to have_content '10.00%'
+    expect(page).to have_content discount_1.quantity_threshold
+  end 
+end
