3156ffee5db63ecb05e1dd794167d94d1595b523
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-12 14:15:11 -0600

    Create merchant discounts index with nav link in dash

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       resources :items
       resources :invoices, only: [:index, :show]
       resources :invoice_items, only: :update
+      resources :discounts, only: [:index]
     end
   end
 end
