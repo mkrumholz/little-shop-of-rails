c3a341ddb2cfac08eb4f91eecf70fb0384c0c5dc
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-13 13:22:56 -0600

    Set up PORO for formatting holiday info

---
+++ app/poros/nager_holiday.rb
@@ -0,0 +1,15 @@
+class NagerHoliday
+  def self.next_3_holidays
+    next_3 = NagerService.holiday_info[0..2]
+    delete_columbus(next_3)
+  end
+
+  private
+
+  def self.delete_columbus(holidays)
+    holidays.each do |holiday|
+      holiday[:localName] = "Indigenous Peoples' Day" if holiday[:localName] == 'Columbus Day'
+    end
+    holidays
+  end
+end
--- spec/poros/nager_holiday_spec.rb
+++ spec/poros/nager_holiday_spec.rb
@@ -3,26 +3,23 @@ require 'rails_helper'
 RSpec.describe NagerHoliday do
   describe '.next_3_holidays' do
     it 'returns the name and date of the next 3 US public holidays' do
-      allow(NagerService).to receive(:holiday_info).and_return({
+      allow(NagerService).to receive(:holiday_info).and_return([{
         date: '2021-07-05',
         localName: 'Independence Day'
-      },
-      {
+      }, {
         date: '2021-09-06',
         localName: 'Labor Day'
-      }, 
-      {
+      }, {
         date: '2021-10-11',
         localName: 'Columbus Day'
-      },
-      {
+      },{
         date: '2021-11-11',
         localName: 'Veterans Day'
-      })
+      }])
 
-      holiday_1 = {date: '2021-07-05', 'Independence Day'}
-      holiday_2 = {date: '2021-09-06', 'Labor Day'}
-      holiday_3 = {date: '2021-10-11', name: "Indigenous Peoples' Day"}
+      holiday_1 = {date: '2021-07-05', localName: 'Independence Day'}
+      holiday_2 = {date: '2021-09-06', localName: 'Labor Day'}
+      holiday_3 = {date: '2021-10-11', localName: "Indigenous Peoples' Day"}
 
       expect(NagerHoliday.next_3_holidays).to eq([holiday_1, holiday_2, holiday_3])
     end
