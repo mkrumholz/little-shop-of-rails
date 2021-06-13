f61650f6ece415b1af18278294a7ca2d816f8529
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-13 12:02:51 -0600

    Create service for calling holiday info from date.nager.at

---
+++ app/services/nager_service.rb
@@ -0,0 +1,8 @@
+class NagerService
+  def self.holiday_info
+    response = Faraday.get 'https://date.nager.at/api/v2/NextPublicHolidays/US'
+      
+    body = response.body
+    JSON.parse(body, symbolize_names: true)
+  end
+end
