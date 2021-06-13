10f3459b301005006e7707c87f3986f0cbb69c79
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-13 10:06:16 -0600

    Remove comment

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -316,12 +316,3 @@ RSpec.describe 'Dashboard' do
     end
   end
 end
-
-# As a merchant
-# When I visit my merchant dashboard
-# Then I see a link to view all my discounts
-# When I click this link
-# Then I am taken to my bulk discounts index page
-# Where I see all of my bulk discounts including their
-# percentage discount and quantity thresholds
-# And each bulk discount listed includes a link to its show page
