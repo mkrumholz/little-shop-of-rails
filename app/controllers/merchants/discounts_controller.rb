9d513dcca7839584784cf95d44fc6a1e41e4d24d
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-12 19:59:51 -0600

    Create functionality to delete merchant discounts

--- app/controllers/merchants/discounts_controller.rb
+++ app/controllers/merchants/discounts_controller.rb
@@ -39,6 +39,12 @@ class Merchants::DiscountsController < ApplicationController
     end
   end
 
+  def destroy
+    discount = Discount.find(params[:id])
+    discount.destroy
+    redirect_to merchant_discounts_path(@merchant.id)
+  end
+
   def set_merchant
     @merchant = Merchant.find(params[:merchant_id])
   end
