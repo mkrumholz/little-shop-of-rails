bb033f8e0f65ffbdde7351bc07b6ca3262922e49
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-14 11:03:38 -0600

    Set up tests for merchant/invoice_item relation

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe InvoiceItem do
   describe 'relationships' do
     it { should belong_to(:invoice) }
     it { should belong_to(:item) }
+    it { should belong_to(:invoice_item).through(:item)}
   end
 
   describe 'enums' do
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Merchant do
   describe 'relationships' do
     it { should have_many(:items).dependent(:destroy) }
     it { should have_many(:discounts).dependent(:destroy) }
+    it { should have_many(:invoice_items).through(:items)}
   end
 
   describe 'validations' do
